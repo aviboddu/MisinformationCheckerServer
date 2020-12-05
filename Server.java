6c24a36b1cff510770de14c812ef60f82b844e52
aviboddu <aviboddu@gmail.com>
2020-12-04 18:10:35 -0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -14,7 +14,7 @@ public class Server {
         MisinformationClassifier classifier = new MisinformationClassifier("pythonWebScraper/Database.csv");
         // Create an HttpServer instance on $PORT accepting up to 100 concurrent connections
         HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(System.getenv("PORT"))), 100);
-		HttpExchange.getResponseHeaders.add("Access-Control-Allow-Origin","*");
+		HttpExchange.getResponseHeaders().add("Access-Control-Allow-Origin","*");
         // Return the index.html file when the browser asks (only for the web app, wouldn't be used for Chrome Extension)
         server.createContext("/", (HttpExchange t) -> {
             String html = Files.readString(Paths.get("index.html"));
